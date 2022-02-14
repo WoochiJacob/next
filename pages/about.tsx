cafa088a4aff44df77d967cde7127f35c8863a5d
Jaocb <43773159+WoochiJacob@users.noreply.github.com>
2022-02-14 16:43:37 +0900

    :poop: next 설치

---
+++ pages/about.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const About: NextPage = () => {
+    return <div>fjdsklfjadslk;fjsa;lkjfdl;k</div>;
+};
+
+export default About;
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next';
+import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
+
+const Home: NextPage = () => {
+    const router = useRouter();
+
+    return <Container>sjfkaldsjflkasd;j</Container>;
+};
+
+const Container = styled.section`
+    width: 1280px;
+    margin: 0 auto;
+`;
+
+export default Home;
