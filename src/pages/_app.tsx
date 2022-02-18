548b7ec3b486c2db10dd71331f6e7c939ee681c1
Jaocb <43773159+WoochiJacob@users.noreply.github.com>
2022-02-18 14:41:40 +0900

    :poop: Global css Emotion으로 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
-import '@styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layouts from '@components/layout/Layouts';
+import { Global } from '@emotion/react';
+import globalStyles from '../styles/globalStyles';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <Layouts>
+            <Global styles={[globalStyles]} />
             <Component {...pageProps} />
         </Layouts>
     );
---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,64 @@
+import { css } from '@emotion/react';
+
+const globalStyles = css`
+  @font-face {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 400;
+    src: url("https://fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.woff2")
+        format("woff2"),
+      url("https://fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.woff")
+        format("woff"),
+      url("https://fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.otf")
+        format("opentype");
+    font-display: swap;
+  }
+  @font-face {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 500;
+    src: url("https://fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Medium.woff2")
+        format("woff2"),
+      url("https://fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Medium.woff")
+        format("woff"),
+      url("https://fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Medium.otf")
+        format("opentype");
+    font-display: swap;
+  }
+  @font-face {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 700;
+    src: url("https://fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Bold.woff2")
+        format("woff2"),
+      url("https://fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Bold.woff")
+        format("woff"),
+      url("https://fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Bold.otf")
+        format("opentype");
+    font-display: swap;
+  }
+
+  body,
+  html {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    font-family: "Noto Sans KR", sans-serif;
+    font-weight: 400;
+  }
+
+  #__next {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+
+  *,
+  :after,
+  :before {
+    box-sizing: border-box;
+  }
+`;
+
+export default globalStyles;
